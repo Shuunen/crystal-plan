ebf40f47d2f8aa183233b46a9634c98de3a96904
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-10-11 18:07:55 +0200

    :sparkles: added storage logic with draft

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,61 +1,57 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>Hello {{ name }} !</h1>
+    <input type="text" v-model="newName" @change="setDraft" placeholder="new name">
+    <p :class="[draftStatus]">Status of new name : {{ draftStatus }}</p>
+    <button @click="applyDraft">Apply</button>
+    <button @click="clearDraft">Clear</button>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'HelloWorld',
-  props: {
-    msg: String,
+  name: 'hello-world',
+  data() {
+    return {
+      name: '',
+      newName: '',
+      draftStatus: ''
+    }
   },
+  created: function() {
+    this.name = Vue.$storage.get('name', 'John (default)')
+    this.newName = Vue.$storage.get('newName', '')
+    this.draftStatus = Vue.$storage.get('draftStatus', 'none')
+  },
+  methods: {
+    clearDraft(){
+      this.newName = ''
+      this.draftStatus = 'none'
+      this.persist()
+    },
+    setDraft(){
+      this.draftStatus = 'draft'
+      this.persist()
+    },
+    applyDraft(){
+      console.log('update name to', this.newName)
+      this.name = this.newName
+      this.newName = ''
+      this.draftStatus = 'persisted'
+      this.persist()
+    },
+    persist(){
+      Vue.$storage.set('name', this.name)
+      Vue.$storage.set('newName', this.newName)
+      Vue.$storage.set('draftStatus', this.draftStatus)
+    }
+  }
 });
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss">
+.draft { color: orange }
+.persisted { color: olivedrab }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import Vue2Storage from 'vue2-storage'
+
+Vue.use(Vue2Storage, {
+  prefix: 'crystalplan_',
+  driver: 'local',
+  ttl: 60 * 60 * 24 * 1000 * 360
+})
 
 Vue.config.productionTip = false
 
